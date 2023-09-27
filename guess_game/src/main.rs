ce0d293dc034cdf87b7478e788e722ff0a6a85e1
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-09-27 13:41:18 +0300

    done making know basic understanding of match,letstd::cmp:: Oreder

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,13 +1,24 @@
 extern crate rand;
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 fn main() {
     let secret_number=rand::thread_rng().gen_range(0, 101);
-    println!("the secrete number is {}",secret_number);
     println!("Guesing Game");
-    println!("Enter Number: ");
-    let mut input=String::new();
-    io::stdin().read_line(&mut input)
+    loop{
+        println!("Enter Number: ");
+        let mut input=String::new();
+        io::stdin().read_line(&mut input)
                 .expect("Falied to get number");
-    println!("You guessed: {}", input);
+        println!("You guessed: {}", input);
+        let guess: i32=input.trim().parse().expect("Please Enter Number Type");
+        match guess.cmp(&secret_number){
+            Ordering::Less=> println!("Less"),
+            Ordering::Greater=>println!("Greater"),
+            Ordering::Equal=>{
+                println!("You Win");
+                break;
+            }
+        }
+    }
 }
