2279f6cb02f3c155fdc3938e04f96264736382d6
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-09-28 17:02:25 +0300

    fuc me rust lsp stops working

--- give_numbers/src/main.rs
+++ give_numbers/src/main.rs
@@ -1,21 +1,17 @@
 use std::io;
-use std::cmp::Ordering;
+//use std::cmp::Ordering;
 fn main() {
     println!("Give there Numbers to order");
     let mut arr=[0,0,0];
     let mut input=String::new();
-    io::stdin().read_line(&mut input).expect("try again.");
-    arr[0]=input.trim().parse().expect("Enter Number");
-    io::stdin().read_line(&mut input).expect("try again.");
-    arr[1]=input.trim().parse().expect("Enter Number");
-    io::stdin().read_line(&mut input).expect("try again.");
-    arr[2]=input.trim().parse().expect("Enter Number");
     let i=0;
     loop{
+        io::stdin().read_line(&mut input).expect("try again.");
+        arr[i]=input.trim().parse().expect("Enter Number");
         if i==3 {
             break;
         }
         println!("Array Values: {}",arr[i]);
-        let i=i+1;
+        let _i=i+1;
     }
 }
