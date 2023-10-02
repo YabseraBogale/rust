6552b750135463e40bdc22e30be8f46c8727f3f8
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-02 19:15:46 +0300

    need further investegation

--- Even_Odd/src/main.rs
+++ Even_Odd/src/main.rs
@@ -1,12 +1,12 @@
 extern crate rand;
 use rand::Rng;
 fn main() {
-    let mut i=0;
+    
     loop{
-        println!("Random number {}",rand::thread_rng().gen_range(0, 6));
-        i+=1;
-        if i==6{
+        let num=rand::thread_rng().gen_range(0, 7);
+        if num%2==0{
             break;
         }
+        println!("Random number {}",num);
     }
 }
