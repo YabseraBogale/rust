81bb8482f50aa1db1517d7736cac013614133122
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-03 17:36:22 +0300

    ok

---
+++ enum_ip/target/debug/deps/enum_ip-0aaa4173df8ab825.d
@@ -0,0 +1,5 @@
+/home/yabsera/Documents/rust/enum_ip/target/debug/deps/enum_ip-0aaa4173df8ab825: src/main.rs
+
+/home/yabsera/Documents/rust/enum_ip/target/debug/deps/enum_ip-0aaa4173df8ab825.d: src/main.rs
+
+src/main.rs:
