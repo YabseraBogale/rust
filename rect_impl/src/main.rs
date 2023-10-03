831004e78b196fdaf7bea465e60e3bcd54b2799e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-03 15:07:01 +0300

    go go poer rangers

--- rect_impl/src/main.rs
+++ rect_impl/src/main.rs
@@ -2,10 +2,11 @@
 struct Rect{
     w:f64,
     h:f64,
+    area:f64,
 }
 
 impl Rect{
-    fn area(&self)-> f64{
+    fn Area(&self)-> f64{
         self.w*self.h
     }
 }
@@ -15,7 +16,13 @@ fn main() {
     let rect_1=Rect{
         w:5.6,
         h:5.9,
+        area:0.0,
     };
     println!("Rectangle: {:#?}",rect_1);
-    println!("Rectangle area : {:?}",rect_1.area());
+    let rect_1=Rect{
+        area:rect_1.Area(),
+       ..rect_1
+        };
+    println!("Rectangle: {:#?}",rect_1);
+    println!("Rectangle area : {:?}",rect_1.area);
 }
