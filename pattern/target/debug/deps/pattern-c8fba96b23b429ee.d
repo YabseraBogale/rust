c417948f718e9b016d2933a425aa530b11eb430b
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-04 17:36:17 +0300

    ok patterns matckinh needs some work

---
+++ pattern/target/debug/deps/pattern-c8fba96b23b429ee.d
@@ -0,0 +1,5 @@
+/media/yabsera/New Volume/github/rust/pattern/target/debug/deps/pattern-c8fba96b23b429ee: src/main.rs
+
+/media/yabsera/New Volume/github/rust/pattern/target/debug/deps/pattern-c8fba96b23b429ee.d: src/main.rs
+
+src/main.rs:
---
+++ pattern/target/debug/pattern.d
@@ -0,0 +1,1 @@
+/media/yabsera/New\ Volume/github/rust/pattern/target/debug/pattern: /media/yabsera/New\ Volume/github/rust/pattern/src/main.rs
