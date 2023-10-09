15afcdd6669a9af4759bb8f38b7f993e00a505cb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-09 17:48:15 +0300

    i know feels like i lost everythinkg i know

---
+++ gene/target/debug/deps/gene-83c9a295b165de5e.d
@@ -0,0 +1,5 @@
+/media/yabsera/New Volume/github/rust/gene/target/debug/deps/gene-83c9a295b165de5e.rmeta: src/main.rs
+
+/media/yabsera/New Volume/github/rust/gene/target/debug/deps/gene-83c9a295b165de5e.d: src/main.rs
+
+src/main.rs:
---
+++ gene/target/debug/deps/gene-899d0cbd7ea6229e.d
@@ -0,0 +1,5 @@
+/media/yabsera/New Volume/github/rust/gene/target/debug/deps/gene-899d0cbd7ea6229e.rmeta: src/main.rs
+
+/media/yabsera/New Volume/github/rust/gene/target/debug/deps/gene-899d0cbd7ea6229e.d: src/main.rs
+
+src/main.rs:
