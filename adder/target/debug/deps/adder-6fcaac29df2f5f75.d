f52a04a7a6a3422e372b39a5cdb97a20cddb6dfc
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-13 20:18:17 +0300

    adder

---
+++ adder/target/debug/deps/adder-6fcaac29df2f5f75.d
@@ -0,0 +1,7 @@
+/media/yabsera/New Volume/github/rust/adder/target/debug/deps/adder-6fcaac29df2f5f75.rmeta: src/lib.rs
+
+/media/yabsera/New Volume/github/rust/adder/target/debug/deps/libadder-6fcaac29df2f5f75.rlib: src/lib.rs
+
+/media/yabsera/New Volume/github/rust/adder/target/debug/deps/adder-6fcaac29df2f5f75.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-f12a4ff0ee820d36.d
@@ -0,0 +1,5 @@
+/media/yabsera/New Volume/github/rust/adder/target/debug/deps/adder-f12a4ff0ee820d36: src/lib.rs
+
+/media/yabsera/New Volume/github/rust/adder/target/debug/deps/adder-f12a4ff0ee820d36.d: src/lib.rs
+
+src/lib.rs:
